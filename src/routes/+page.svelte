307bfdc0707815609b0bf9cb0aba4a19ff2f585b
Joshua Duque <josheze.duque@gmail.com>
2024-08-31 22:29:57 -0500

    Added basic song search

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<p class="font-bold">render-test</p>
-<p>hello world</p>
+<script lang="ts">
+	let songTitle: string;
+	let songsJson: any;
+
+	async function searchSong(title: string) {
+		if (!title) return;
+
+		const url = `https://api.animethemes.moe/song?include=animethemes.anime&filter[title-like]=%${title}%`;
+		try {
+			const response = await fetch(url);
+			if (!response.ok) {
+				console.log(`Response status: ${response.status}`);
+				return;
+			}
+
+			const json = await response.json();
+			console.log(json);
+			songsJson = json;
+		} catch (error: any) {
+			console.error(error.message);
+		}
+	}
+</script>
+
+<input
+	bind:value={songTitle}
+	class="shadow"
+	placeholder="Song name"
+	type="text"
+	name="song-name"
+	id=""
+/>
+<button
+	on:click={() => searchSong(songTitle)}
+	class="bg-blue-500 hover:bg-blue-700 text-white rounded"
+	type="button">Search Song</button
+>
+
+{#if songsJson && songsJson.songs.length < 1}
+	<p>No songs found!</p>
+{:else if songsJson}
+	{#each songsJson.songs as song}
+		<p>{song.title}</p>
+	{/each}
+{:else}
+	<p>Search for a song!</p>
+{/if}
